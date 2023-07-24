9236bffb0fe369afdf0e684aa3c10dfb063e61e8
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-24 12:59:04 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,16 @@
 #!/usr/bin/python3
-import requests
+"""script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests as r
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    employee_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-
-    try:
-        # Fetch employee information
-        response = requests.get(employee_url)
-        response.raise_for_status()
-        employee_data = response.json()
-
-        # Fetch employee's TODO list
-        response = requests.get(todos_url)
-        response.raise_for_status()
-        todos_data = response.json()
-
-        # Calculate progress
-        total_tasks = len(todos_data)
-        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-        # Display progress
-        print(f"Employee {employee_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
-        for todo in todos_data:
-            if todo['completed']:
-                print("\t", todo['title'])
-
-    except requests.exceptions.HTTPError as errh:
-        print("HTTP Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print("Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print("Error:", err)
-
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
