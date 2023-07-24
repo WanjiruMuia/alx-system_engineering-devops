38da87161e54d4c534036ffef1f6c1a94cbd54a0
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-24 12:53:44 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,46 @@
 
-n script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+import requests
+import sys
 
-from requests import get
-from sys import argv
+def get_employee_todo_progress(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
 
+    try:
+        # Fetch employee information
+        response = requests.get(employee_url)
+        response.raise_for_status()
+        employee_data = response.json()
 
-if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+        # Fetch employee's TODO list
+        response = requests.get(todos_url)
+        response.raise_for_status()
+        todos_data = response.json()
 
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+        # Calculate progress
+        total_tasks = len(todos_data)
+        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
+        # Display progress
+        print(f"Employee {employee_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
+        for todo in todos_data:
+            if todo['completed']:
+                print("\t", todo['title'])
 
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Error:", err)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    for i in tasks:
-        print("\t {}".format(i))
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
