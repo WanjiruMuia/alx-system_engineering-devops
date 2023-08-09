9166e323cb2635b72ef63741c0c355814875f42c
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-08-09 11:53:26 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
-"""
-
 import requests
 
-
-def recurse(subreddit, hot_list=[], after=""):
-    """
-    Queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-
-    - If not a valid subreddit, return None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"after": after},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            hot_list.append(title)
-        after = req.json().get("data").get("after")
-
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
+def recurse(subreddit, hot_list=[], after=None):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Your User Agent'}  # Replace with your user agent
+    
+    if after:
+        params = {'after': after}
     else:
+        params = None
+    
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 404:
         return None
+    
+    data = response.json()
+    
+    if 'data' in data and 'children' in data['data']:
+        children = data['data']['children']
+        for child in children:
+            hot_list.append(child['data']['title'])
+        
+        if data['data']['after']:
+            recurse(subreddit, hot_list, data['data']['after'])
+    
+    return hot_list
+
+subreddit_name = 'python'  # Replace with the subreddit you want to query
+hot_articles = recurse(subreddit_name)
+if hot_articles is not None:
+    for idx, title in enumerate(hot_articles, start=1):
+        print(f"{idx}. {title}")
+else:
+    print(f"Subreddit '{subreddit_name}' not found or invalid.")
