7cf4ac8a6db89a738d19628b3ef2dd906e04865c
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-08-09 11:54:55 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
-def recurse(subreddit, hot_list=[], after=None):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Your User Agent'}  # Replace with your user agent
-    
-    if after:
-        params = {'after': after}
+
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        params = None
-    
-    response = requests.get(url, headers=headers, params=params)
-    
-    if response.status_code == 404:
-        return None
-    
-    data = response.json()
-    
-    if 'data' in data and 'children' in data['data']:
-        children = data['data']['children']
-        for child in children:
-            hot_list.append(child['data']['title'])
-        
-        if data['data']['after']:
-            recurse(subreddit, hot_list, data['data']['after'])
-    
-    return hot_list
-
-subreddit_name = 'python'  # Replace with the subreddit you want to query
-hot_articles = recurse(subreddit_name)
-if hot_articles is not None:
-    for idx, title in enumerate(hot_articles, start=1):
-        print(f"{idx}. {title}")
-else:
-    print(f"Subreddit '{subreddit_name}' not found or invalid.")
+        return (None)
